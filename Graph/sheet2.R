8f654374f890ddd754496854feadf6a436008e9d
evanzhu2013 <evan.zhu2013@gmail.com>
2015-09-02 19:21:52 +0800

    change color

--- Graph/sheet2.R
+++ Graph/sheet2.R
@@ -9,7 +9,7 @@ sheet2 = data.frame(group,relative,se)
 
 p = ggplot(sheet2,aes(x=factor(group),y=relative))+geom_bar(stat='identity',width=0.4,fill='grey',colour='black')+xlab('')+ylab('Relative expression')+theme_light()+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.title.y=element_text(size=10),panel.border=element_blank(),axis.line=element_line(colour='grey',size=0.5))+geom_errorbar(aes(ymin=relative-se,ymax=relative+se),width=0.1,color='black',size=0.35)
 
-p+annotate('text',x='rapamycin',y=0.03,label='*',size=6,col='red')
+p+annotate('text',x='rapamycin',y=0.03,label='*',size=6,col='black')
 
 rel = c(1.0792,0.8526,0.9526,0.0208,0.0221,0.0186)
 group = factor(rep(c('control','rapamycin'),each=3))
