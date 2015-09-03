179caf3c93c573f53a3fe178e003d4eadd4a86a6
evanzhu2013 <evan.zhu2013@gmail.com>
2015-09-03 14:37:49 +0800

    change linetype

--- Graph/sheet4.R
+++ Graph/sheet4.R
@@ -6,4 +6,4 @@ sheet4 = read.csv('/Users/Evan/DataScience/Working/Graph/csv/sheet4.csv',header=
 
 sheet4$group = factor(rep(c(1:3),each=5))
 
-ggplot(sheet4,aes(x=factor(Inhibition.time),y=Concentration,group=group,linetype=group,shape=group))+geom_line()+geom_point(size=3)+theme_light()+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.title.y=element_text(size=12),panel.border=element_blank(),axis.line=element_line(colour='grey',size=0.5))+xlab('Inhibition time')
+ggplot(sheet4,aes(x=factor(Inhibition.time),y=Concentration,group=group,linetype=group,shape=group))+geom_line(colour='grey')+geom_point(size=3)+theme_light()+theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),axis.title.y=element_text(size=12),panel.border=element_blank(),axis.line=element_line(colour='grey',size=0.5))+xlab('Inhibition time')
